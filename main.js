098d20f5d517c33bab44d97bbe44b8321bc4a243
Mario-Nader <mario.nader.mn.m@gmail.com>
2026-02-02 01:14:54 +0200

    deployment updates

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 const cookieParser = require('cookie-parser')
 const express = require('express')
 require('dotenv').config()
-const port = process.env.port;
+const port = process.env.PORT
 const DBcontrollers = require('./database')
 const rateLimit = require('express-rate-limit')
 const taskRouter = require('./routes/TasksRoutes')
 const authRouter = require('./routes/AuthRoutes')
 const app = express()
 
-
-let limiter = rateLimit({
-    max:100,
-    windowMs:60*60*1000,
-    message:"you have reached the limit of requests, Please try again after one hour"
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "you have reached the limit of requests, Please try again after one hour"
 })
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
-
-
-app.use(limiter())
+app.use(limiter)
 app.use(cookieParser())
 app.use(express.json())
-app.use('/auth',authRouter)
-app.use('/task',taskRouter)
+
+app.use('/auth', authRouter)
+app.use('/task', taskRouter)
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
